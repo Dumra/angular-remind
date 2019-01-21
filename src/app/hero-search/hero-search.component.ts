9b900743d5400e74c38d8989b01f62744509ed18
Dumra <d.shuliakov@gmail.com>
2019-01-21 20:37:50 +0200

    Fixed

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -5,31 +5,34 @@ import { HeroService } from '../hero.service';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-hero-search',
-  templateUrl: './hero-search.component.html',
-  styleUrls: ['./hero-search.component.css']
+    selector: 'app-hero-search',
+    templateUrl: './hero-search.component.html',
+    styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
 
-  heroes$: Observable<Hero[]>;
-  private searchTerms = new Subject<string>();
-  constructor(private heroService: HeroService) { }
+    heroes$: Observable<Hero[]>;
+    private searchTerms = new Subject<string>();
 
-  ngOnInit() {
-    this.heroes$ = this.searchTerms.pipe(
-        // wait 300ms after each keystroke before considering the term
-        debounceTime(300),
+    constructor(private heroService: HeroService) {
+    }
 
-        // ignore new term if same as previous term
-        distinctUntilChanged(),
+    // Push a search term into the observable stream.
+    search(term: string): void {
+        this.searchTerms.next(term);
+    }
 
-        // switch to new search observable each time the term changes
-        switchMap((term: string) => this.heroService.searchHeroes(term)),
-    );
-  }
+    ngOnInit(): void {
+        this.heroes$ = this.searchTerms.pipe(
+            // wait 300ms after each keystroke before considering the term
+            debounceTime(300),
 
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
+            // ignore new term if same as previous term
+            distinctUntilChanged(),
+
+            // switch to new search observable each time the term changes
+            switchMap((term: string) => this.heroService.searchHeroes(term)),
+        );
+    }
 
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,19 +7,21 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
 export class HeroService {
 
-  constructor(
-      private http: HttpClient,
-      private messageService: MessageService
-  ) { }
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService
+    ) {
+    }
 
     private heroesUrl = 'api/heroes';
+
     private log(message: string) {
-      this.messageService.add(`HeroService: ${message}`);
+        this.messageService.add(`HeroService: ${message}`);
     }
 
     // getHeroes(): Observable<Hero[]> {
@@ -43,35 +45,35 @@ export class HeroService {
             tap(_ => this.log(`hetched hero id=${id}`)),
             catchError(this.handleError<Hero>(`getHero id=${id}`))
         );
-      return of(HEROES.find(hero => hero.id === id));
+        return of(HEROES.find(hero => hero.id === id));
     }
 
     /** PUT: update the hero on the server */
     updateHero(hero: Hero): Observable<any> {
         const httpOptions = {
-            headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+            headers: new HttpHeaders({'Content-Type': 'application/json'})
         };
         return this.http.put(this.heroesUrl, hero, httpOptions)
             .pipe(
-              tap(_ => this.log(`udapted hero id=${hero.id}`)),
-              catchError(this.handleError<any>('updatedHero'))
+                tap(_ => this.log(`udapted hero id=${hero.id}`)),
+                catchError(this.handleError<any>('updatedHero'))
             );
     }
 
     addHero(hero: Hero): Observable<any> {
         const httpOptions = {
-            headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+            headers: new HttpHeaders({'Content-Type': 'application/json'})
         };
         return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
             .pipe(
-              tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+                tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
                 catchError(this.handleError<Hero>('addHero'))
             );
     }
 
     deleteHero(hero: Hero | number): Observable<any> {
         const httpOptions = {
-            headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+            headers: new HttpHeaders({'Content-Type': 'application/json'})
         };
         const id = typeof hero === 'number' ? hero : hero.id;
         const url = `${this.heroesUrl}/${id}`;
@@ -83,15 +85,16 @@ export class HeroService {
             );
     }
 
+    /* GET heroes whose name contains search term */
     searchHeroes(term: string): Observable<Hero[]> {
         if (!term.trim()) {
             // if not search term, return empty hero array.
             return of([]);
-            return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
-                tap(_ => this.log(`found heroes matching "${term}"`)),
-                catchError(this.handleError<Hero[]>('searchHeroes', []))
-            );
         }
+        return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+            tap(_ => this.log(`found heroes matching "${term}"`)),
+            catchError(this.handleError<Hero[]>('searchHeroes', []))
+        );
     }
 
     /**
@@ -100,7 +103,7 @@ export class HeroService {
      * @param operation - name of the operation that failed
      * @param result - optional value to return as the observable result
      */
-    private handleError<T> (operation = 'operation', result?: T) {
+    private handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
 
             // TODO: send the error to remote logging infrastructure
